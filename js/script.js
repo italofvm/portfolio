5808bf22abfe56a64e09cafafd91ed05977303df
italofvm <italofernandesvm@gmail.com>
2023-07-05 19:15:40 -0300

    Envio de email js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,24 @@
+const form = document.getElementById("myForm");
 
-async function enviarFormulario(event) {
-  event.preventDefault(); // Impede o envio do formulário
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-  // Obtém os valores dos campos
-  var nome = document.getElementById("nome").value;
-  var email = document.getElementById("email").value;
-  var assunto = document.getElementById("assunto").value;
-  var mensagem = document.getElementById("mensagem").value;
+  const formData = new FormData(form);
 
-  // Realiza a validação dos campos
-  if (nome.trim() === "") {
-    alert("Por favor, preencha o campo Nome");
-    return;
-  }
-
-  if (email.trim() === "") {
-    alert("Por favor, preencha o campo Email");
-    return;
-  }
-
-if (nome === '' || email === '' || assunto === '' || mensagem === '') {
-    alert('Por favor, preencha todos os campos.');
-    return;
-  }
-
-  // Se todas as validações passarem, você pode enviar o formulário aqui
-  alert('Formulário enviado com sucesso!');
-  // Adicione o código para enviar o formulário para o servidor aqui...
-
-  // Limpa os campos do formulário
-  document.getElementById('nome').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('assunto').value = '';
-  document.getElementById('mensagem').value = '';
-}
+  fetch("https://formsubmit.co/ajax/italofernandesvm@gmail.com", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.success) {
+          // O envio do formulário foi bem-sucedido
+          // Redireciona para a página de sucesso
+          window.location.href = "sucesso.html";
+        }
+    })
+    .catch((error) => {
+      // Lida com erros
+      console.error(error);
+    });
+});
